e81bd541a060b3471475a3e4d6758a27c23bd592
unknown <User@Z-Dell-E5470.ecs.soton.ac.uk>
2018-03-10 23:43:20 +0000

    Updating location class2

--- app/src/main/java/com/example/sjw2g15/absolutebustard/Location.java
+++ app/src/main/java/com/example/sjw2g15/absolutebustard/Location.java
@@ -1,8 +1,17 @@
 package com.example.sjw2g15.absolutebustard;
 
+import java.io.Serializable;
+
 /**
  * Created by User on 10/03/2018.
  */
 
-public class Location {
+public class Location implements Serializable {
+    public double lat;
+    public double lon;
+
+    public Location(double lat, double lon){
+        this.lat = lat;
+        this.lon = lon;
+    }
 }
